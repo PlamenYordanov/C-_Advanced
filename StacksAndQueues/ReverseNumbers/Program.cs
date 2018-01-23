c35b4b0cb42e90bbda41cc284f454e5d68a92369
PlamenYordanov <yordanov.plamen@abv.bg>
2018-01-23 18:56:42 +0200

    test

---
+++ StacksAndQueues/ReverseNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReverseNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
