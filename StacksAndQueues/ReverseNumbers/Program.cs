cce1aa0192521b6768c8a1fe2702d3dd80f8586d
PlamenYordanov <yordanov.plamen@abv.bg>
2018-01-25 19:01:32 +0200

    stack fibonacci 100/100

--- StacksAndQueues/ReverseNumbers/Program.cs
+++ StacksAndQueues/ReverseNumbers/Program.cs
@@ -9,31 +9,21 @@
         public static void Main()
         {
             int n = int.Parse(Console.ReadLine());
-            var fibMemory = EmptyMemory();
-            
-            Console.WriteLine(Fib(n, fibMemory));
-        }
-        private static int Fib(int n, int[] fibMemory)
-        {
-            if (n <= 1)
+            var stack = new Stack<long>();
+            long a = 0;
+            long b = 1;
+            long c;
+            stack.Push(a);
+            stack.Push(b);
+            for (int i = 2; i < n + 1; i++)
             {
-                return n;
+                c = a + b;
+                a = b;
+                b = c;
+                stack.Push(c);
             }
-            if (fibMemory[n] != -1)
-            {
-                return fibMemory[n];
-            }
-            fibMemory[n] = Fib(n - 1, fibMemory) + Fib(n - 2, fibMemory);
-            return fibMemory[n];
+            Console.WriteLine(stack.Peek());
         }
-        private static int[] EmptyMemory()
-        {
-            int[] memory = new int[51];
-            for (int i = 0; i < 51; i++)
-            {
-                memory[i] = -1;
-            }
-            return memory; 
-        }
+        
     }
 }
