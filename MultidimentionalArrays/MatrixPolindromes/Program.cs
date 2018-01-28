6c33ee80866545830ba8eba42ce857a2e29d1b25
PlamenYordanov <yordanov.plamen@abv.bg>
2018-01-28 09:35:14 +0200

    matrix polindromes without the matrix 100/ 100

---
+++ MultidimentionalArrays/MatrixPolindromes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MatrixOfPalindromes
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            string[] rowsColumns = Console.ReadLine().Split();
+            int rows = int.Parse(rowsColumns[0]);
+            int columns = int.Parse(rowsColumns[1]);
+
+            for (int r = 0 + 97; r < rows + 97; r++)
+            {
+                for (int c = r; c < columns + r; c++)
+                {
+                    Console.Write("{0}{1}{0} ", (char)r, (char)c);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
