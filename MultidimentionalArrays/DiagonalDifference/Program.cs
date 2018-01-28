8ea54be339308f812df52376dd71e509afa4c86d
PlamenYordanov <yordanov.plamen@abv.bg>
2018-01-28 10:10:14 +0200

    diagonal difference100/100

---
+++ MultidimentionalArrays/DiagonalDifference/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                var row = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                AddRowToMatrix(matrix, row, i);
+            }
+            int left = GetLeftDiagonalSum(matrix);
+            int right = GetRightDiagonalSum(matrix);
+            int absDifference = Math.Abs(left - right);
+            Console.WriteLine(absDifference);
+        }
+        private static int GetLeftDiagonalSum(int[,] matrix)
+        {
+            int sum = 0;
+            int element = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sum += matrix[i, element];
+                element++;
+            }
+            return sum;
+        }
+        private static int GetRightDiagonalSum(int[,] matrix)
+        {
+            int sum = 0;
+            int element = matrix.GetLength(0) -1;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sum += matrix[i, element];
+                element--;
+            }
+            return sum;
+        }
+        private static void AddRowToMatrix(int[,] matrix, int[] elements, int row)
+        {
+            int j = row;
+            for (int i = 0; i < elements.Length; i++)
+            {
+                matrix[j, i] = elements[i];
+            }
+        }
+    }
+}
