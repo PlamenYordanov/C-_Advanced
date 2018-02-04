fb128c9429724d366bb805a5d0c51a26ec236b22
PlamenYordanov <yordanov.plamen@abv.bg>
2018-02-04 10:47:00 +0200

    find evens or odds 100/100

---
+++ FilesAndStreams/SlicingFile/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace SlicingFile
+{
+    using System;
+    using System.IO;
+    
+    public class Program
+    {
+        public static void Main()
+        {
+            int numSplices = int.Parse(Console.ReadLine());
+            using (FileStream readStream = new FileStream(@"..\OddLines\Files\sliceMe.mp4", FileMode.OpenOrCreate))
+            
+            {
+                var size = readStream.Length;
+                var fileChunk = readStream.Length / numSplices;
+                var buffer = new Byte[readStream.Length];
+
+                int offset = 0;
+
+                for (int i = 0; i < numSplices - 1; i++)
+                {
+
+                    string fileName = $"Part-{i}.mp4";
+                    using (FileStream writer = File.OpenWrite($@"..\OddLines\Files\{fileName}"))
+                    {
+                        int bytesRead;
+
+                        while ((bytesRead =
+                                readStream.Read(buffer, 0, fileChunk)) > 0)
+                        {
+                            writer.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                    fileChunk *= 2;
+                }
+
+
+            }
+        }
+        
+        
+    }
+}
---
+++ FunctionalProgramming/ActionPrint/ActionPrint.cs
@@ -0,0 +1,37 @@
+﻿namespace ActionPrint
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ActionPrint
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string evenOrOdd = Console.ReadLine();
+
+            Predicate<int> evenOrOddFinder = (i) => { return i % 2 == 0; };
+            if (evenOrOdd.Equals("odd"))
+            {
+                evenOrOddFinder = (i) => { return i % 2 != 0; };
+            }
+
+            var result = GetEvenOrOddElements(numbers, evenOrOddFinder);
+            Console.WriteLine(string.Join(" ", result));
+        }
+        static IEnumerable<int> GetEvenOrOddElements(int[] numbers, Predicate<int> getEvenOrOdd)
+        {
+            int lowerEnd = numbers[0];
+            int upperEnd = numbers[1];
+
+            for (int i = lowerEnd; i <= upperEnd; i++)
+            {
+                if (getEvenOrOdd(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
